304d2173be184a3237c363541889fef403c3894e
Vahid Khalafov <vahid.khalafovv@gmail.com>
2026-03-12 18:24:15 +0400

    Added \n to print function

--- src/kernel.c
+++ src/kernel.c
@@ -3,9 +3,13 @@
 
 static volatile uint16_t *const VGA_BUFFER = (uint16_t *)0xB8000;
 static const uint8_t VGA_COLOR = 0x0F;
+size_t cursor = 0;
 
 void print(const char *msg) {
     for (size_t i = 0; msg[i] != '\0'; ++i) {
+        if (msg[i] == "\n"){
+            cursor += 80 - (cursor % 80);
+        }
         VGA_BUFFER[i] = (uint16_t)msg[i] | ((uint16_t)VGA_COLOR << 8);
     }
 }
