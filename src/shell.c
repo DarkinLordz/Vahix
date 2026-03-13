e06842fd3919e096a1ae3f494d1dd5a356bc3178
Vahid Khalafov <vahid.khalafovv@gmail.com>
2026-03-14 00:29:13 +0400

    Added more commands

--- src/shell.c
+++ src/shell.c
@@ -9,6 +9,19 @@ void execute_command(char *command){
         print_string("Commands: help, clear\n");
     } else if (strcmp(command, "clear") == 0){
         clear();
+    } else if (strcmp(command, "fetch") == 0){
+        print_string("#          # OS:\n #        # Host:\n  #      # Kernel:\n   #    # Uptime:\n    #  # Shell:\n     ## Display:\n");
+    } else if (strcmp(command, "exit") == 0){
+        outw(0x604, 0x2000);
+        outw(0x4004, 0x3400);
+        outw(0xB004, 0x2000);
+        print_string("It is now safe to turn off your computer.");
+    
+        __asm__ volatile ("cli");
+        while(1) {
+            __asm__ volatile ("hlt");
+        }
+}
     }
 }
 
